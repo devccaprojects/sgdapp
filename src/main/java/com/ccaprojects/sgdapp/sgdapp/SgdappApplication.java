fdcfeb9a7e701168066f0f959d087e96b406909b
Christian Luque Vera <luqueve@gmail.com>
2018-11-28 20:26:08 -0500

    Creación del proyecto

---
+++ src/main/java/com/ccaprojects/sgdapp/sgdapp/SgdappApplication.java
@@ -0,0 +1,12 @@
+package com.ccaprojects.sgdapp.sgdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgdappApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ccaprojects/sgdapp/sgdapp/SgdappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ccaprojects.sgdapp.sgdapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SgdappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
